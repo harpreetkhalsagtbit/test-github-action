37bb1ecff0429f09ed735f6f00954e4a6b02668f
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2021-11-03 16:41:04 +0530

    fixed all

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var k = 5;
-console.log('hey')
+// console.log('hey')
 
 var j = {
+    value:k,
     name:'jarr', age: 10
 }
-
+function test(variable) {
+    return variable;
+}
+test(j)
 // x.y()
