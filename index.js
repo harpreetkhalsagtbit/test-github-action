31d02df3a77a91a5c484374cf8a53ee36d537742
harpreetAdPushup <harpreet.singh@adpushup.com>
2021-11-09 15:13:04 +0530

    udpate

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ function test(variable) {
 test(j);
 console.log(j);
 x.y();
-console.log('I am merged into dev automatically - enough');
+console.log('I am merged into dev automatically -push test adp');
